9eeb49be5c8dedf4bfa0f2f672b2871990fb7673
JASEEM <muhdjaseemtk@gmail.com>
2025-10-07 10:08:23 +0530

    Cart complete

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -47,12 +47,24 @@ export const addCart = async (req, res) => {
 };
 
 
+export const deleteProductInCart = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const { productId } = req.body;
 
-export const deleteCart = async(req,res) =>{
-    try{
- 
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch(error){
 
-    }
-}
+    cart.items = cart.items.filter(
+      (item) => item.productId.toString() !== productId
+    );
+
+    await cart.save();
+    res.status(200).json({ message: "Product removed", cart });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Error " });
+  }
+};
